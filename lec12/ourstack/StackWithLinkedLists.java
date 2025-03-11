80771d0b1f7882efa52b35d6fea6478900581939
Katie Keith <kakeith406@gmail.com>
2025-03-11 09:26:28 -0400

    update

---
+++ lec12/ourstack/StackWithLinkedLists.java
@@ -0,0 +1,93 @@
+/**
+ * Uses a singly-linked list to implement a Stack 
+ */
+
+package ourstack; 
+
+public class StackWithLinkedLists<Item> implements Stack<Item>{
+    // We'll keep these private since we just want the stack operations 
+    private Node first;     // top of stack
+    private int numItems;   // size of the stack
+
+    // Inner Node class 
+    private class Node {
+        private Item data;
+        private Node next;
+
+        private Node(){
+            this.data = null; 
+            this.next = null; 
+        }
+    }
+
+    /**
+     * Constructor 
+     * Initializes the empty stack 
+     */
+    public StackWithLinkedLists() {
+        first = null;
+        numItems = 0; 
+    }
+
+    
+    /**
+     * @return true if this stack is empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * @return the number of items in this stack
+     */
+    public int size() {
+        return this.numItems;
+    }
+
+    /**
+     * Adds the item to this stack (adds at the beginning of the LinkedList)
+     * O(1)
+     *
+     * @param  item the item to add
+     */
+    public void push(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.data = item;
+        first.next = oldfirst;
+        numItems++;
+    }
+
+    /**
+     * Removes and returns the item most recently added to this stack
+     * (at the beginning of the LinkedList)
+     * 
+     * O(1)
+     *
+     * @return the item most recently added
+     */
+    public Item pop() {
+        if (isEmpty()){
+            System.out.println("Stack is empty"); 
+            return null; 
+        }
+        Item item = first.data;        // save item to return
+        first = first.next;            // delete first node
+        numItems--;
+        return item;                   // return the saved item
+    }
+
+    public static void main(String[] args) {
+        // Testing 
+        StackWithLinkedLists<String> stack = new  StackWithLinkedLists<String>();
+            
+        // Push some onto the stack
+        stack.push("Webpage visited at 9am"); 
+        stack.push("Webpage visited at 10am"); 
+        stack.push("Webpage visited at 12pm");
+
+        while(!stack.isEmpty()){
+            System.out.println("Back button to:"+stack.pop());
+        }
+    }
+}
